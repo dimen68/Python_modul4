58ace0c3d2b87d0555dffab063dace463aea3bd1
bochar68 <bochar.dg@gmail.com>
2024-10-13 15:28:34 +0300

    закрепить навык создания и импортирования модулей

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+# Модули и пакеты
+from fake_math import divide as fd
+from true_math import divide as td
+
+result1 = fd(38, 3)
+result2 = fd(9, 0)
+result3 = td(149, 7)
+result4 = td(25, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
